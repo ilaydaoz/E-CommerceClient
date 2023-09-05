b9625c43c6ab46027504372a1bdcdb84d10e5c7a
ilayda <ilaydaozken@gmail.com>
2023-09-05 22:19:38 +0300

    .

--- src/app/admin/components/customer/customer.module.ts
+++ src/app/admin/components/customer/customer.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerComponent } from './customer.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,15 @@ import { CustomerComponent } from './customer.component';
     CustomerComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {path: "", component: CustomerComponent}
+    ])
   ]
 })
 export class CustomerModule { }
+
+/* forChild: özel bir modül için rotaları tanımlamanıza yardımcı olan bir yöntemdir.
+   forRoot:  ana rotalarını ve rotaların nasıl çalışacağını tanımlamak için kullanılan bir yöntemdir.
+   uygulamanızın hangi sayfalarının hangi URL'lere eşleştiğini kontrol eder.
+*/
