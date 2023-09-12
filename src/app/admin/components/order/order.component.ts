1e984048ab17325bbd9c5c084f3eb92f852c93b0
ilayda <ilaydaozken@gmail.com>
2023-09-12 19:25:16 +0300

    Order spinner

--- src/app/admin/components/order/order.component.ts
+++ src/app/admin/components/order/order.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
-export class OrderComponent implements OnInit {
-  constructor(private spinner: NgxSpinnerService) { }
+export class OrderComponent extends BaseComponent implements OnInit {
 
+  constructor(spinner: NgxSpinnerService) {
+    super(spinner);
+  }
   ngOnInit(): void {
-    this.spinner.show("S1");
-
-    setTimeout(() => {
-      this.spinner.hide("S1");
-    }, 5000);
+    this.showSpinner(SpinnerType.BallScaleMultiple)
   }
 }
