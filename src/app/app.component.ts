07af839312bac3be384b36d2fefecfbe3a9f91a6
ilayda <ilaydaozken@gmail.com>
2023-09-12 17:45:26 +0300

    ui Toastr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $:any
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ declare var $:any
 })
 export class AppComponent {
   title = 'E-CommerceClient';
+  constructor(private toastrService: CustomToastrService) {
+      toastrService.message("merhaba","ilayda", {
+        messageType:ToastrMessageType.Info,
+        position: ToastrPosition.TopCenter
+      });
+      toastrService.message("merhaba","ilayda", {
+        messageType:ToastrMessageType.Success,
+        position: ToastrPosition.TopCenter
+      });
+      toastrService.message("merhaba","ilayda", {
+        messageType:ToastrMessageType.Error,
+        position: ToastrPosition.TopCenter
+      });
+      toastrService.message("merhaba","ilayda", {
+        messageType:ToastrMessageType.Warning,
+        position: ToastrPosition.TopCenter
+      });
+  }
 }
