9c2b6d01b5da0a3c567fc2639dd6a4b14a2fd3bf
ilayda <ilaydaozken@gmail.com>
2023-09-14 17:03:44 +0300

    get post put delete test

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -10,13 +10,35 @@ import { HttpClientService } from 'src/app/services/common/http-client.service';
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
 
-  constructor(spinner: NgxSpinnerService, private httpClientService:HttpClientService ) {
+  constructor(spinner: NgxSpinnerService, private httpClientService: HttpClientService) {
     super(spinner);
   }
   ngOnInit(): void {
     this.showSpinner(SpinnerType.Pacman);
+
     this.httpClientService.get({
-      controller:"products"
-    }) .subscribe(data =>console.log(data));
-   }
+      controller: "products"
+    }).subscribe(data => console.log(data));
+
+    // this.httpClientService.post({
+    //   controller:"products"
+    // },{
+    //    name:"kalem",
+    //    stock:"1000",
+    //    price:2.50,
+    // }).subscribe();
+
+    // this.httpClientService.put({
+    //   controller: "products",
+    // }, {
+    //   id: "e956fc75-5b97-494f-8ede-2ecf932eed6a",
+    //   name: "renkli kağıt",
+    //   stock: 1500,
+    //   price: 5.5
+    // }).subscribe();
+
+   //this.httpClientService.delete({
+   //  controller: "products",
+   //}, "7d9cd1cb-cb36-4ab0-9463-ee17cd130f50").subscribe();
+  }
 }
