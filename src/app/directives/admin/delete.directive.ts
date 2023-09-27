ad4c570857b9aff9369a5dd24a2a4b21b94196b7
ilayda <ilaydaozken@gmail.com>
2023-09-27 16:47:24 +0300

    Delete directive de düzenleme

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,6 +1,11 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerType } from 'src/app/base/base.component';
 import { DeleteDialogsComponent, DeleteState } from 'src/app/dialogs/delete-dialogs/delete-dialogs.component';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 declare var $: any;
 @Directive({
@@ -10,8 +15,10 @@ export class DeleteDirective {
 
   constructor(private element: ElementRef,
     private _renderer: Renderer2,
-    private productservice: ProductService,
-    public dialog: MatDialog
+    private httpClientService: HttpClientService,
+    public dialog: MatDialog,
+    private alertifyService : AlertifyService,
+    private spinner: NgxSpinnerService,
   ) {
     const img = _renderer.createElement("img");
     img.setAttribute("src", "/assets/delete.png");
@@ -22,20 +29,37 @@ export class DeleteDirective {
   }
 
   @Input() id: string;
+  @Input() controller: string;
   @Output() callback: EventEmitter<any> = new EventEmitter();
   @HostListener("click")
 
   async onclick() {
     this.openDialog(async () => {
       const td: HTMLImageElement = this.element.nativeElement;
-      await this.productservice.delete(this.id)
-      $(td.parentElement).animate({
-        opacity:0,
-        left:"+=50",
-        height:"toogle"
-      },700,()=>{
-        this.callback.emit();
-      })
+      // await this.productservice.delete(this.id)
+      this.httpClientService.delete({
+        controller: this.controller
+      }, this.id).subscribe(data => {
+        $(td.parentElement).animate({
+          opacity: 0,
+          left: "+=50",
+          height: "toogle"
+        }, 700, () => {
+          this.callback.emit();
+          this.alertifyService.message(" ürün başarıyla silinmiştir.", {
+            dismissOthers: true,
+            messageType: MessageType.Success,
+            position: Position.TopRight
+          })
+        });
+      },(errorRespone :HttpErrorResponse)=>{
+        this.spinner.hide(SpinnerType.BallSpinClockwiseFadeRotating);
+        this.alertifyService.message(" ürün silirken hata oluştu.", {
+          dismissOthers: true,
+          messageType: MessageType.Error,
+          position: Position.TopRight
+        })
+      });
     });
   }
 
